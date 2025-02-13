0387b13e173143be8330db026f66a2eb3ca0cca8
yolophg <phg5590@gmail.com>
2025-02-13 00:01:42 -0500

    chore: configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',  // export static HTML
+  images: {
+    unoptimized: true,
+  },
+  basePath: "",
+  assetPrefix: "",
 };
 
 export default nextConfig;
